a493b8bf4ff6e5615bed403d5e3d9988245b65c3
JJWallis <josh.jamesonwallis@gmail.com>
2022-05-29 13:38:35 +0100

    reducer setup in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from './styles/Theme'
 import CtWrapper from './styles/ContainerWrapper'
 import GlobalStyles from './styles/GlobalStyles'
 import Comments from './Comments'
+
+function getGlobalState() {}
+
+function reducer(initialState, action) {
+   switch (action.type) {
+      case 'ADD_COMMENT':
+         return {
+            ...initialState,
+            comments: [...initialState.comments, action.payload],
+         }
+      default:
+         return initialState
+   }
+}
+
+const [comments, dispatch] = useReducer(reducer, [], getGlobalState)
 
 const App: React.FC = () => {
    return (
